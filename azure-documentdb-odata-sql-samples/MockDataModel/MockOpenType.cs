7c050fbe6b671a72f2af6f14b5972698514f8336
Aboo <azarnoush@gmail.com>
2018-02-06 15:04:35 +1100

    Document Formatting
    - added some document formatiing
    - added tags to the mocked model

--- azure-documentdb-odata-sql-samples/MockDataModel/MockOpenType.cs
+++ azure-documentdb-odata-sql-samples/MockDataModel/MockOpenType.cs
@@ -6,50 +6,56 @@ using Newtonsoft.Json;
 
 namespace azure_documentdb_odata_sql_tests
 {
-    [DataContract]
-    public enum MockEnum
-    {
-        [EnumMember]
-        ZERO,
+	[DataContract]
+	public enum MockEnum
+	{
+		[EnumMember]
+		ZERO,
 
-        [EnumMember]
-        ONE,
+		[EnumMember]
+		ONE,
 
-        [EnumMember]
-        TWO
-    }
+		[EnumMember]
+		TWO
+	}
 
 
-    [DataContract]
-    public class MockOpenType : Document
-    {
-        /// <summary>
-        /// The property name for EnglishName
-        /// </summary>
-        public const string EnglishNamePropertyName = "englishName";
+	[DataContract]
+	public class MockOpenType : Document
+	{
+		/// <summary>
+		/// The property name for EnglishName
+		/// </summary>
+		public const string EnglishNamePropertyName = "englishName";
 
-        /// <summary>
-        /// The property name for EnumNumber
-        /// </summary>
-        public const string EnumNumberPropertyName = "enumNumber"; 
+		/// <summary>
+		/// The property name for EnumNumber
+		/// </summary>
+		public const string EnumNumberPropertyName = "enumNumber";
 
-        /// <summary>
-        /// EnglishName
-        /// </summary>
-        [DataMember(Name = EnglishNamePropertyName)]
-        [JsonProperty(PropertyName = EnglishNamePropertyName)]
-        public string EnglishName { get; set; }
+		public const string TagsPropertyName = "tags";
 
-        /// <summary>
-        /// EnumNumber
-        /// </summary>
-        [DataMember(Name = EnumNumberPropertyName)]
-        [JsonProperty(PropertyName = EnumNumberPropertyName)]
-        public MockEnum EnumNumber { get; set; }
+		/// <summary>
+		/// EnglishName
+		/// </summary>
+		[DataMember(Name = EnglishNamePropertyName)]
+		[JsonProperty(PropertyName = EnglishNamePropertyName)]
+		public string EnglishName { get; set; }
 
-        /// <summary>
-        /// PropertyBag to make Edm open-typed
-        /// </summary>
-        public Dictionary<string, object> PropertyBag { get; set; }
-    }
+		/// <summary>
+		/// EnumNumber
+		/// </summary>
+		[DataMember(Name = EnumNumberPropertyName)]
+		[JsonProperty(PropertyName = EnumNumberPropertyName)]
+		public MockEnum EnumNumber { get; set; }
+
+		/// <summary>
+		/// PropertyBag to make Edm open-typed
+		/// </summary>
+		public Dictionary<string, object> PropertyBag { get; set; }
+
+		[DataMember(Name = TagsPropertyName)]
+		[JsonProperty(PropertyName = TagsPropertyName)]
+		public List<string> Tags { get; set; }
+	}
 }
